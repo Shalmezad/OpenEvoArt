37a0355d7b34cbd5d8745422b48d69ba176973bf
Richard Wardin <richardw@hesonline.com>
2017-06-18 12:20:20 -0400

    Tested code to write image

--- lib/openevoart.rb
+++ lib/openevoart.rb
@@ -1,7 +1,13 @@
-
+require 'rubygems'
+require 'chunky_png'
 
 def main
   puts "Hello world"
+  # Let's test writing an image:
+  # From the chunky_png documentation
+  png = ChunkyPNG::Image.new(16,16, ChunkyPNG::Color::TRANSPARENT)
+  png[1,1] = ChunkyPNG::Color.rgba(10,20,30,128)
+  png.save('out/test.png')
 end
 
 
