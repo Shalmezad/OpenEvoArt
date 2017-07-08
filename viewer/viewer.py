e972d545fb7ce17ea9aa0c804e71752a0f863afe
Richard Wardin <richardw@hesonline.com>
2017-07-08 14:51:05 -0400

    Full screen viewer

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -11,7 +11,8 @@ half_height = height/2
 pygame.font.init()
 myfont = pygame.font.SysFont("monospace",15, True)
 
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((800,600), pygame.FULLSCREEN)
+surf = pygame.Surface((width,height))
 done = False
 #test_image = 'out/art/1.png'
 test_image_file = max(glob.iglob('out/art/*.png'),key = os.path.getctime)
@@ -45,6 +46,9 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                done = True
     test_image_file = max(glob.iglob('out/art/*.png'),key = os.path.getctime)
     if test_image_file != cur_image_file :
         cur_image_file = test_image_file
@@ -54,25 +58,28 @@ while not done:
                              1, (255,255,255))
 
     # Background:
-    screen.blit(bg_image,(0,0))
+    surf.blit(bg_image,(0,0))
 
     # Github info
-    screen.blit(github_label,(10,10))
-    screen.blit(github_label2,(10,24))
+    surf.blit(github_label,(10,10))
+    surf.blit(github_label2,(10,24))
     # File label
-    screen.blit(file_label, (10, height-20))
+    surf.blit(file_label, (10, height-20))
 
     # Border:
-    pygame.draw.rect(screen,(255,255,255),
+    pygame.draw.rect(surf,(255,255,255),
                      pygame.Rect(image_x-2,
                                  image_y-2,
                                  image_width+4,
                                  image_height+4))
-    pygame.draw.rect(screen,(0,0,0),
+    pygame.draw.rect(surf,(0,0,0),
                      pygame.Rect(image_x-1,
                                  image_y-1,
                                  image_width+2,
                                  image_height+2))
     # Image
-    screen.blit(image, (image_x, image_y))
+    surf.blit(image, (image_x, image_y))
+    screen.blit(pygame.transform.scale(surf,(800,600)), (0,0))
+    #screen.blit(surf,(0,0))
     pygame.display.flip()
+pygame.quit()
